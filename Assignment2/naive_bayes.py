e56274a522f21e088e2b159d38e71aab7b140b79
danielyoon-maker <58200952+danielyoon-maker@users.noreply.github.com>
2021-09-25 23:06:32 -0700

    Updates heading

--- Assignment2/naive_bayes.py
+++ Assignment2/naive_bayes.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Daniel Yoon
+# FILENAME: naive_bayes.py
+# SPECIFICATION: fufills assignment requirements
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 20 minutes
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
